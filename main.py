1862cabb9c478cba5ba1ed14155e2edd6e54f00b
MohammadrezaAmani <More.Amani@yahoo.com>
2023-12-10 11:52:31 +0330

    fix known bugs

---
+++ main.py
@@ -0,0 +1,13 @@
+from kakuro import Game
+
+board = """
+#     #     #     24|#  16|#
+#     #     6|17  ?     ?
+#     17|18 ?     ?     ?
+#|17  ?     ?     ?     #
+#|11  ?     ?     #     #
+"""
+
+if __name__ == "__main__":
+    game = Game(board)
+    game.run(depth=3)
